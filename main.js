c8f0d12406cfa27d81caf9ba1dda8424e4b4007d
GamerAum <77730710+GamerAum@users.noreply.github.com>
2023-01-07 12:03:06 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+play1 = localStorage.getItem("player_1");
+play2 = localStorage.getItem("player_2");
+pscore1 = 0;
+pscore2 = 0;
+document.getElementById("player_name1").innerHTML = play1 + ":" + pscore1;
+document.getElementById("player_name2").innerHTML = play2 + ":" + pscore2;
+
+
